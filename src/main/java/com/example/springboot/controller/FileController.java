e3b39fabdb707911195adb476b55368fa29f4175
Mr He <studenthe@outlook.com>
2021-12-02 21:36:03 +0800

    添加文件下载

---
+++ src/main/java/com/example/springboot/controller/FileController.java
@@ -0,0 +1,33 @@
+package com.example.springboot.controller;
+
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+@RestController
+@RequestMapping("/file")
+public class FileController {
+
+    @RequestMapping(value = "/media")
+    public ResponseEntity<InputStreamResource> downloadFile() throws IOException {
+        String filePath = "D:\\Study\\GitHub\\SpringBoot\\log\\info.log";
+        FileSystemResource file = new FileSystemResource(filePath);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getFilename()));
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentLength(file.contentLength())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new InputStreamResource(file.getInputStream()));
+    }
+}
